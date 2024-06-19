957f335268e71bd0d11ecc2cfe9229c09780f31e
Sanjay Kumar M <sanjaymoad786@gmail.com>
2024-06-19 12:12:41 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 class LeaveApplication(models.Model):
     employee_name = models.CharField(max_length=100, default='')
     employee_id = models.CharField(max_length=100, default='')
+    slack_user_id = models.CharField(max_length=100, default='')  # New field for Slack user ID
     employment_type = models.CharField(
         max_length=20, 
         choices=[('full_time', 'Full Time'), ('intern', 'Intern')], 
@@ -25,7 +26,7 @@ class LeaveApplication(models.Model):
     manager_id = models.CharField(max_length=100, default='')
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    submitted_at = models.DateTimeField(auto_now_add=True) 
+    submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.employee_name} - {self.start_date} to {self.end_date}"
