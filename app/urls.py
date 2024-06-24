4c84d9cf07e193905d101f1c4f59a26ac51de4ab
Sanjay Kumar M <sanjaymoad786@gmail.com>
2024-06-24 11:59:04 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('slack/events/', views.slack_events, name='slack_events'),
     path('slack/slash_command/', views.slack_slash_command, name='slack_slash_command'),
     path('slack/interactions/', views.handle_interactions, name='handle_interactions'),  
+]
